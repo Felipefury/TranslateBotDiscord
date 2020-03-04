c4012c526636ce5b7243b7e493ee525e2d17786b
Felipe GM <bebefofo123abc@gmail.com>
2020-03-03 22:58:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Criado por Felipe GM, Api usada http://api.whatsmate.net
-var http = require('http');
-const Discord = require('discord.js');
+var http = require("http");
+const Discord = require("discord.js");
 const client = new Discord.Client();
  
 var fromLang = "en";
@@ -8,20 +8,21 @@ var toLang = "pt";
 var clientId = "FREE_TRIAL_ACCOUNT";
 var clientSecret = "PUBLIC_SECRET";
 
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", () => {
+  console.log("Logged in as ", client.user.tag);
 });
  
-client.on('message', message => {
-    if(message.author.bot) return
-    if(message.channel.id == 'ID DO CANAL') {
-    text = message.content
+client.on("message", message => {
+    if(message.author.bot || message.channel.id !== "CHANNEL ID") {
+        return;
+    }
+
+    let text = message.content;
 
     var jsonPayload = JSON.stringify({
-        fromLang: fromLang,
-        toLang: toLang,
-        text: text
+        "fromLang": fromLang,
+        "toLang": toLang,
+        "text": text
     });
 
     var options = {
@@ -41,28 +42,27 @@ client.on('message', message => {
     var request = new http.ClientRequest(options);
     request.end(jsonPayload);
     
-    request.on('response', function (response) {
-        console.log('Status code: ' + response.statusCode);
-        response.setEncoding('utf8');
-        response.on('data', function (chunk) {
-            console.log(text + " -> " + chunk);
+    request.on("response", function (response) {
+        console.log("Status code: " + response.statusCode);
 
-            if(text == chunk) return
+        response.setEncoding("utf8");
+        response.on("data", function (chunk) {
+
+            if(text == chunkhunk) {
+                return;
+            };
 
             const embed = new Discord.RichEmbed()
             .setTitle("Criado por Felipe GM")
             .setAuthor("TranslateBOT", "https://cdn.discordapp.com/avatars/319606741639757824/9de3604b01582ad852d586c29cfdd7d0.jpg?size=1024")
             .setColor(0x00AE86)
-
-            .addField("Original, en: ",text)
+            .addField("Original, en: ", text)
             .setTimestamp()
-            .addField("Tradução pt: ",chunk)
-            .setFooter("Pedido: " + message.author.username, message.author.avatarURL)
+            .addField("Tradução, pt: ", chunk)
+            .setFooter("Pedido: " + message.author.username, message.author.avatarURL);
             message.channel.send({embed});
         });
     });
-
-    }
 })
 //API COM TODAS OS IDIOMAS SUPORTADOS http://api.whatsmate.net/v1/translation/supported-codes
 client.login("TOKEN");
